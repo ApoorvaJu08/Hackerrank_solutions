441d22255eed19ecd4a3b585c3983896f2855ba0
ApoorvaJu08 <apoorva10339@gmail.com>
2019-03-14 14:37:09 +0530

    Solution to Alphaet_Rngoli added

---
+++ Python/Alphabet_Rangoli.py
@@ -0,0 +1,91 @@
+# You are given an integer, . Your task is to print an alphabet rangoli of size . (Rangoli is a form of Indian folk art based on creation of patterns.)
+
+# Different sizes of alphabet rangoli are shown below:
+
+# #size 3
+
+# ----c----
+# --c-b-c--
+# c-b-a-b-c
+# --c-b-c--
+# ----c----
+
+# #size 5
+
+# --------e--------
+# ------e-d-e------
+# ----e-d-c-d-e----
+# --e-d-c-b-c-d-e--
+# e-d-c-b-a-b-c-d-e
+# --e-d-c-b-c-d-e--
+# ----e-d-c-d-e----
+# ------e-d-e------
+# --------e--------
+
+# #size 10
+
+# ------------------j------------------
+# ----------------j-i-j----------------
+# --------------j-i-h-i-j--------------
+# ------------j-i-h-g-h-i-j------------
+# ----------j-i-h-g-f-g-h-i-j----------
+# --------j-i-h-g-f-e-f-g-h-i-j--------
+# ------j-i-h-g-f-e-d-e-f-g-h-i-j------
+# ----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+# --j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+# j-i-h-g-f-e-d-c-b-a-b-c-d-e-f-g-h-i-j
+# --j-i-h-g-f-e-d-c-b-c-d-e-f-g-h-i-j--
+# ----j-i-h-g-f-e-d-c-d-e-f-g-h-i-j----
+# ------j-i-h-g-f-e-d-e-f-g-h-i-j------
+# --------j-i-h-g-f-e-f-g-h-i-j--------
+# ----------j-i-h-g-f-g-h-i-j----------
+# ------------j-i-h-g-h-i-j------------
+# --------------j-i-h-i-j--------------
+# ----------------j-i-j----------------
+# ------------------j------------------
+# The center of the rangoli has the first alphabet letter a, and the boundary has the  alphabet letter (in alphabetical order).
+
+# Input Format
+
+# Only one line of input containing , the size of the rangoli.
+
+# Output Format
+
+# Print the alphabet rangoli in the format explained above.
+
+# Sample Input
+
+# 5
+# Sample Output
+
+# --------e--------
+# ------e-d-e------
+# ----e-d-c-d-e----
+# --e-d-c-b-c-d-e--
+# e-d-c-b-a-b-c-d-e
+# --e-d-c-b-c-d-e--
+# ----e-d-c-d-e----
+# ------e-d-e------
+# --------e--------
+
+import string
+alpha = string.ascii_lowercase
+
+def print_rangoli(n):
+    L = []
+
+    for i in range(n):
+        s = "-".join(alpha[i:n])
+        L.append(s[::-1]+s[1:])
+
+    width = len(L[0])
+
+    for i in range(n-1, 0, -1):
+        print(L[i].center(width, "-"))
+        
+    for i in range(n):
+        print(L[i].center(width, "-"))
+    
+if __name__ == '__main__':
+    n = int(input())
+    print_rangoli(n)
