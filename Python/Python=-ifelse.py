f1c7a5e864172941b22f90c136d59130082859d2
ApoorvaJu08 <apoorva10339@gmail.com>
2019-03-25 18:02:22 +0530

    If-else solution added

---
+++ Python/Python=-ifelse.py
@@ -0,0 +1,50 @@
+# Task 
+# Given an integer, , perform the following conditional actions:
+
+# If  is odd, print Weird
+# If  is even and in the inclusive range of  to , print Not Weird
+# If  is even and in the inclusive range of  to , print Weird
+# If  is even and greater than , print Not Weird
+# Input Format
+
+# A single line containing a positive integer, .
+
+# Constraints
+
+# Output Format
+
+# Print Weird if the number is weird; otherwise, print Not Weird.
+
+# Sample Input 0
+
+# 3
+# Sample Output 0
+
+# Weird
+# Explanation 0
+
+ 
+#  is odd and odd numbers are weird, so we print Weird.
+
+# Sample Input 1
+
+# 24
+# Sample Output 1
+
+# Not Weird
+# Explanation 1
+
+ 
+#  and  is even, so it isn't weird. Thus, we print Not Weird.
+
+
+N = int(input())
+if(N%2 != 0):
+    print("Weird")
+else:
+    if(N>=2 and N <= 5):
+        print("Not Weird")
+    elif(N >= 6 and N <= 20):
+        print("Weird")
+    elif(N > 20):
+        print("Not Weird")
